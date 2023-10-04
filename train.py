a7d12400067e28ff133d11a103aecb02a9109594
FreddieC19 <freddieclark334@gmail.com>
2023-10-04 18:00:55 -0400

    train.py now functional, using GPU causes crashing

--- train.py
+++ train.py
@@ -1,90 +1,104 @@
 import argparse
 import torch
 import torch.nn as nn
-import torch.utils.data as data
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 from pathlib import Path
-from tqdm import tqdm
 from tensorboardX import SummaryWriter
-from torchvision import transforms
 from custom_dataset import custom_dataset
 from AdaIN_net import AdaIN_net, encoder_decoder
 
-# Define command-line arguments
-parser = argparse.ArgumentParser(description="Train AdaIN Style Transfer Model")
-parser.add_argument("-content_dir", type=str, default="datasets/COCO1K/", help="Path to content images directory")
-parser.add_argument("-style_dir", type=str, default="datasets/WikiArt1k/", help="Path to style images directory")
-parser.add_argument("-gamma", type=float, default=1.0, help="Alpha blending parameter for AdaIN")
-parser.add_argument("-e", type=int, default=20, help="Number of epochs")
-parser.add_argument("-b", type=int, default=20, help="Batch size")
-parser.add_argument("-encoder_name", type=str, default="encoder.pth", help="Encoder model name")
-parser.add_argument("-decoder_name", type=str, default="decoder.pth", help="Decoder model name")
-parser.add_argument("-p", type=str, default="decoder.png", help="Path to save sample output image")
-parser.add_argument("-cuda", type=str, default="Y", help="Use CUDA (Y/N)")
-args = parser.parse_args()
-
 def main():
-    # Set device to 'cuda' if available, otherwise 'cpu'
+    # Define command-line arguments
+    parser = argparse.ArgumentParser(description="Train AdaIN Style Transfer Model")
+    parser.add_argument("-content_dir", type=str, default="datasets/COCO1K/", help="Path to content images directory")
+    parser.add_argument("-style_dir", type=str, default="datasets/WikiArt1k/", help="Path to style images directory")
+    parser.add_argument("-gamma", type=float, default=1.0, help="Alpha blending parameter for AdaIN")
+    parser.add_argument("-e", type=int, default=20, help="Number of epochs")
+    parser.add_argument("-b", type=int, default=20, help="Batch size")
+    parser.add_argument("-encoder_name", type=str, default="encoder.pth", help="Encoder model name")
+    parser.add_argument("-decoder_name", type=str, default="decoder.pth", help="Decoder model name")
+    parser.add_argument("-p", type=str, default="decoder.png", help="Path to save sample output image")
+    parser.add_argument("-cuda", type=str, default="Y", help="Use CUDA (Y/N)")
+    args = parser.parse_args()
+
     device = torch.device('cuda' if torch.cuda.is_available() and args.cuda == "Y" else 'cpu')
 
-    # Create a directory to save model checkpoints and logs
+    sample_path = "samples/"
     save_dir = Path('./experiments')
     save_dir.mkdir(exist_ok=True, parents=True)
-    log_dir = Path('./logs')
-    log_dir.mkdir(exist_ok=True, parents=True)
-    writer = SummaryWriter(log_dir=str(log_dir))
+    sample_dir = Path(sample_path)
+    sample_dir.mkdir(exist_ok=True, parents=True)
 
-    # Load the encoder from the provided file
-    encoder = encoder_decoder.encoder
-    encoder.load_state_dict(torch.load(args.encoder_name, map_location=device))
+    model = AdaIN_net(encoder_decoder.encoder, encoder_decoder.decoder)
+    model = model.to(device)
+    model.train()
 
-    # Load the decoder from the provided file (if available)
-    if Path(args.decoder_name).exists():
-        decoder = encoder_decoder.decoder
-        decoder.load_state_dict(torch.load(args.decoder_name, map_location=device))
-    else:
-        # Create a new decoder with random weights if not provided
-        decoder = encoder_decoder.decoder
+    encoder_state_dict = torch.load(args.encoder_name, map_location=device)
+    model.encoder.load_state_dict(encoder_state_dict)
 
-    decoder = decoder.to(device)
-    decoder.train()
-
-    # Define the content transformation (you can customize this)
     transform = transforms.Compose([
         transforms.Resize(size=(512, 512)),
         transforms.RandomCrop(256),
         transforms.ToTensor()
     ])
 
-    # Create a custom dataset for content images
+    style_dataset = custom_dataset(dir=args.style_dir, transform=transform)
+    style_loader = DataLoader(style_dataset, batch_size=args.b, shuffle=True, num_workers=16)
+
     content_dataset = custom_dataset(dir=args.content_dir, transform=transform)
-    content_loader = data.DataLoader(content_dataset, batch_size=args.b, shuffle=True, num_workers=16)
+    content_loader = DataLoader(content_dataset, batch_size=args.b, shuffle=True, num_workers=16)
 
-    # Define the optimizer
-    optimizer = torch.optim.Adam(decoder.parameters(), lr=1e-4)
+    optimizer = optim.Adam(model.decoder.parameters(), lr=1e-4)
 
-    # Training loop
+    log_dir = Path('./logs')
+    log_dir.mkdir(exist_ok=True, parents=True)
+    writer = SummaryWriter(log_dir=str(log_dir))
+
+    print("Start Training on ",device)
     for epoch in range(args.e):
-        for i, content_images in enumerate(content_loader):
-            content_images = content_images.to(device)
+        for i, (content_images, style_images) in enumerate(zip(content_loader, style_loader)):
+            content_images, style_images = content_images.to(device), style_images.to(device)
 
-            # Forward pass through the decoder
-            output = decoder(content_images)
+            content_features = model.encode(content_images)
+            style_features = model.encode(style_images)
+            output = model.decode(content_features[-1])
 
-            # Calculate the reconstruction loss (MSE loss)
-            loss = torch.nn.MSELoss()(output, content_images)
+            loss_reconstruction = torch.nn.MSELoss()(output, content_images)
+
+            style_loss = 0.0
+            for content_feat, style_feat in zip(content_features, style_features):
+                style_loss += model.style_loss(content_feat, style_feat)
+            num_elements = content_features[-1].size(1) * content_features[-1].size(2)
+            style_loss /= num_elements
+
+            style_loss_weight = 1e-6
+            loss = loss_reconstruction + style_loss_weight * style_loss
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            writer.add_scalar('loss_reconstruction', loss.item(), epoch * len(content_loader) + i + 1)
+            print(f'Epoch [{epoch + 1}/{args.e}], Step [{i + 1}/{len(content_loader)}], 'f'Reconstruction Loss: {loss_reconstruction.item():.4f}, Style Loss: {style_loss.item():.4f}')
 
-            # Save the decoder's weights periodically
-            if (epoch * len(content_loader) + i + 1) % 10000 == 0 or (epoch == args.e - 1 and i == len(content_loader) - 1):
-                state_dict = decoder.state_dict()
+            writer.add_scalar('loss_reconstruction', loss_reconstruction.item(), epoch * len(content_loader) + i + 1)
+            writer.add_scalar('loss_style', style_loss.item(), epoch * len(content_loader) + i + 1)
+
+            if (epoch * len(content_loader) + i + 1) % 10000 == 0 or (
+                    epoch == args.e - 1 and i == len(content_loader) - 1):
+                state_dict = model.decoder.state_dict()
                 for key in state_dict.keys():
                     state_dict[key] = state_dict[key].to(torch.device('cpu'))
-                torch.save(state_dict, save_dir / f'decoder_iter_{epoch * len(content_loader) + i + 1}.pth.tar')
+                torch.save(state_dict, save_dir / f'decoder_iter_{epoch * len(content_loader) + i + 1}.pth')
+
+            if (epoch * len(content_loader) + i + 1) % 1000 == 0:
+                with torch.no_grad():
+                    sample_output = model.decode(content_features[-1])
+                    sample_output = sample_output.clamp(0, 1)
+                    sample_filename = sample_dir / f'sample_epoch_{epoch}_iter_{i}.png'
+                    torchvision.utils.save_image(sample_output, sample_filename, nrow=8)
 
     writer.close()
 
